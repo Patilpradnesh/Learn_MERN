82e5c0401092aac69434678382e1657d8d780878
patilpradnesh <patilpradnesh4124@gmail.com>
2025-08-08 16:30:28 +0530

    changes in serverr

--- server/server.js
+++ server/server.js
@@ -6,9 +6,11 @@ const app = express();
 const ConnectDb = require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware.js");
 const cors =require('cors');
-
 var corsOptions={
-  origin: true,  // Allows all origins
+  origin: [
+    'http://localhost:5173',                    // Local development
+    'https://learn-mern-rouge.vercel.app'       // Your Vercel frontend
+  ],
   methods:"GET,POST,PUT,DELETE,PATCH,HEAD",
   credentials:true,
   optionsSuccessStatus:200
